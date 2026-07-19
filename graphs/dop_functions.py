a0eb268325138055305d434b7a45439012d33ceb
Denis <denisshulgin2017@yandex.ru>
2026-07-19 19:48:50 +0300

    style

--- graphs/dop_functions.py
+++ graphs/dop_functions.py
@@ -1,39 +1,39 @@
 from math import exp
-
 from matplotlib import pyplot as plt
+
 
 def loss(x):
     return (x - 3) ** 2
 
 
-x_values = [value / 10 for value in range(-30, 30)]
+def main():
+    x_values = [value / 10 for value in range(0, 61)]
+    loss_values = [loss(value) for value in x_values]
 
-loss_values = [loss(value) for value in x_values]
+    figure, axis = plt.subplots()
 
-figure, axis = plt.subplots()
+    axis.plot(
+        x_values,
+        loss_values,
+        label="loss(x)"
+    )
 
-axis.plot(
-    x_values,
-    loss_values,
-    label="loss(x)"
-)
+    axis.axhline(
+        0, 
+        color="black",
+        linewidth=0.5
+    )
 
-axis.axhline(
-    0, 
-    color="black",
-    linewidth=0.5
-)
+    axis.axvline(
+        0, 
+        color="black",
+        linewidth=0.5
+    )
 
-axis.axvline(
-    0, 
-    color="black",
-    linewidth=0.5
-)
+    axis.set_xlabel("x")
+    axis.set_ylabel("y")
+    axis.set_title("Functions")
+    axis.grid(True)
+    axis.legend()
 
-axis.set_xlabel("x")
-axis.set_ylabel("y")
-axis.set_title("Functions")
-axis.grid(True)
-axis.legend()
-
-plt.show()
+    plt.show()
--- graphs/plot_functions.py
+++ graphs/plot_functions.py
@@ -1,58 +1,67 @@
 from math import exp
-
 from matplotlib import pyplot as plt
+
 
 def linear(x):
     return 2 * x + 1
 
+
 def quadratic(x):
     return x ** 2
+
 
 def exponential(x):
     return exp(x)
 
-x_values = [value / 10 for value in range(-20, 21)]
 
-linear_values = [linear(value) for value in x_values]
-quadratic_values = [quadratic(value) for value in x_values]
-exponential_values = [exponential(value) for value in x_values]
+def main():
+    x_values = [value / 10 for value in range(-20, 21)]
 
-figure, axis = plt.subplots()
+    linear_values = [linear(value) for value in x_values]
+    quadratic_values = [quadratic(value) for value in x_values]
+    exponential_values = [exponential(value) for value in x_values]
 
-axis.plot(
-    x_values,
-    linear_values,
-    label="2x + 1"
-)
+    figure, axis = plt.subplots()
 
-axis.plot(
-    x_values,
-    quadratic_values,
-    label="x^2"
-)
+    axis.plot(
+        x_values,
+        linear_values,
+        label="2x + 1"
+    )
 
-axis.plot(
-    x_values,
-    exponential_values,
-    label="exp(x)"
-)
+    axis.plot(
+        x_values,
+        quadratic_values,
+        label="x^2"
+    )
 
-axis.axhline(
-    0, 
-    color="black",
-    linewidth=0.5
-)
+    axis.plot(
+        x_values,
+        exponential_values,
+        label="exp(x)"
+    )
 
-axis.axvline(
-    0, 
-    color="black",
-    linewidth=0.5
-)
+    axis.axhline(
+        0, 
+        color="black",
+        linewidth=0.5
+    )
 
-axis.set_xlabel("x")
-axis.set_ylabel("y")
-axis.set_title("Functions")
-axis.grid(True)
-axis.legend()
+    axis.axvline(
+        0, 
+        color="black",
+        linewidth=0.5
+    )
 
-plt.show()
+    axis.set_xlabel("x")
+    axis.set_ylabel("y")
+    axis.set_title("Functions")
+    axis.grid(True)
+    axis.legend()
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
+
