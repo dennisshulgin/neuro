63dc43d7b473b30e15626594bf95b8819c866471
Denis <denisshulgin2017@yandex.ru>
2026-08-02 17:15:30 +0300

    test

--- models/test_evaluation.py
+++ models/test_evaluation.py
@@ -9,7 +9,26 @@ features, targets = generate_linear_dataset(
     seed=42,
 )
 
+weights = np.array([1.0, 1.0])
+bias = 0.0
+
+confident_loss, confident_accuracy = evaluate_binary_classifier(features, targets, weights, bias)
+print(confident_loss, confident_accuracy)
+
+weights = np.array([5.0, 5.0])
+bias = 0.0
+
+confident_loss, confident_accuracy = evaluate_binary_classifier(features, targets, weights, bias)
+print(confident_loss, confident_accuracy)
+
 weights = np.array([0.0, 0.0])
 bias = 0.0
 
-print(evaluate_binary_classifier(features, targets, weights, bias))
+confident_loss, confident_accuracy = evaluate_binary_classifier(features, targets, weights, bias)
+print(confident_loss, confident_accuracy)
+
+weights = np.array([-1.0, -1.0])
+bias = 0.0
+
+confident_loss, confident_accuracy = evaluate_binary_classifier(features, targets, weights, bias)
+print(confident_loss, confident_accuracy)
