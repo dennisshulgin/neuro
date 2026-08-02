c633401aa7dd88885f06532a34bd57851ce9b601
Denis <denisshulgin2017@yandex.ru>
2026-08-02 17:19:06 +0300

    test

--- models/test_evaluation.py
+++ models/test_evaluation.py
@@ -12,23 +12,30 @@ features, targets = generate_linear_dataset(
 weights = np.array([1.0, 1.0])
 bias = 0.0
 
-confident_loss, confident_accuracy = evaluate_binary_classifier(features, targets, weights, bias)
-print(confident_loss, confident_accuracy)
+correct_loss, correct_accuracy = evaluate_binary_classifier(features, targets, weights, bias)
 
 weights = np.array([5.0, 5.0])
 bias = 0.0
 
 confident_loss, confident_accuracy = evaluate_binary_classifier(features, targets, weights, bias)
-print(confident_loss, confident_accuracy)
 
 weights = np.array([0.0, 0.0])
 bias = 0.0
 
-confident_loss, confident_accuracy = evaluate_binary_classifier(features, targets, weights, bias)
-print(confident_loss, confident_accuracy)
+zero_loss, zero_accuracy = evaluate_binary_classifier(features, targets, weights, bias)
 
 weights = np.array([-1.0, -1.0])
 bias = 0.0
 
-confident_loss, confident_accuracy = evaluate_binary_classifier(features, targets, weights, bias)
-print(confident_loss, confident_accuracy)
+wrong_loss, wrong_accuracy = evaluate_binary_classifier(features, targets, weights, bias)
+
+assert np.isclose(correct_accuracy, 1.0)
+assert np.isclose(confident_accuracy, correct_accuracy)
+assert np.isclose(wrong_accuracy, 0.0)
+
+assert wrong_accuracy < zero_accuracy
+assert zero_accuracy < correct_accuracy
+
+assert confident_loss < correct_loss
+assert correct_loss < zero_loss
+assert zero_loss < wrong_loss
